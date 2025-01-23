f7d76edc9730babfc7090963b3d54b81f889f12f
Eduardo <tkd13000@gmail.com>
2025-01-23 05:48:23 -0600

    Title task can be edited

--- editTitle.js
+++ editTitle.js
@@ -5,6 +5,9 @@ editTitleButton.addEventListener("click", () =>
 {
     // Activa la edición del label
     titleTask.contentEditable = true;
+    titleTask.textContent = "";//Delete inner text
+    titleTask.style.width = "150px";
+    titleTask.style.height = "25px";
     titleTask.focus(); // Coloca el cursor en el label
 
     // Opcional: Desactiva la edición al perder el foco
