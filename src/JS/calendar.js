bbf2856593536a82dcf1d568719f8442462c5332
Eduardo <tkd13000@gmail.com>
2025-01-31 06:59:02 -0600

    Working on calendar position

---
+++ src/JS/calendar.js
@@ -0,0 +1,9 @@
+document.getElementById("openModalButton").addEventListener("click", function()
+{
+    document.getElementBy("modal").style.display = "block";
+});
+
+document.getElementById("closeModalButton").addEventListener("click", function()
+{
+    document.getElementById("modal").style.display = "none";
+});
