517998e9f17efe8eb31fcb882c939774a0654450
Eduardo <tkd13000@gmail.com>
2025-02-10 07:04:09 -0600

    When clicking completed button, editContainer is hiiden and listOfTasks is shown

--- src/JS/menuButtons.js
+++ src/JS/menuButtons.js
@@ -1,8 +1,8 @@
-
 const all = document.getElementById("all");
-
+const listOfTasks = document.querySelector(".listOfTasks");
 
 all.addEventListener("click", function()
 {
     editTaskContainer.style.display = "none";
+    listOfTasks.style.display = "flex";
 })
